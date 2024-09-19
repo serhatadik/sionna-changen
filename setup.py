44cad14ddd0dff5000ef30d2262f498ed6c32e48
Serhat Tadik <serhatadik@gmail.com>
2024-09-18 21:42:37 -0400

    specify the sionna version for proper setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'jupyterlab-widgets~=3.0.12',
-    	'sionna',
+    	'sionna==0.18.0',
     	'mitsuba',
     	'drjit',
         'torch',
